014212aca4de1a21036d77d5f29b2192e7949357
Abrar Arshad <abrararshad32@gmail.com>
2026-04-28 19:27:15 +0500

    oka

--- lib/src/map_location_picker.dart
+++ lib/src/map_location_picker.dart
@@ -496,8 +496,8 @@ class _MapLocationPickerState extends State<MapLocationPicker> {
                         ),
                         children: [
                           fm.TileLayer(
-                            urlTemplate:
-                                "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                          urlTemplate:
+    "https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
                             userAgentPackageName: "com.example.app",
                           ),
 
